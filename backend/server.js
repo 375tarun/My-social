e73f262abe3ae0ca72375c2d70bac83f23cce7be
TARUN KUSHWAHA <98316006+375tarun@users.noreply.github.com>
2024-07-23 22:47:24 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-app.use(cors());
+app.use(cors({origin:"https://socailimpact.netlify.app",credentials:true}));
 
 app.use(express.json({ limit: "5mb" })); 
 
