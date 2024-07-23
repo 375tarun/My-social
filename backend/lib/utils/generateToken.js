fb423ec02f210bf9d6ff22faba3df026898bf21d
TARUN KUSHWAHA <98316006+375tarun@users.noreply.github.com>
2024-07-23 23:44:23 +0530

    Update generateToken.js

--- backend/lib/utils/generateToken.js
+++ backend/lib/utils/generateToken.js
@@ -8,7 +8,7 @@ export const generateTokenAndSetCookie = (userId, res) => {
 	res.cookie("jwt", token, {
 		maxAge: 15 * 24 * 60 * 60 * 1000, //MS
 		httpOnly: true, // prevent XSS attacks cross-site scripting attacks,
-		sameSite:"none"
+		sameSite:"none",
 		secure: process.env.NODE_ENV !== "development",
 	});
 };
