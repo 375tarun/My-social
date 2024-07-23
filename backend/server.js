c74a2a529146d0e4fde42b0bd5f28c7390720f50
TARUN KUSHWAHA <98316006+375tarun@users.noreply.github.com>
2024-07-23 22:30:02 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
-
+import cors from "cors";
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
@@ -22,6 +22,8 @@ cloudinary.config({
 const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
+
+app.use(cors());
 
 app.use(express.json({ limit: "5mb" })); 
 
