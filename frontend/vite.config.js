4b641833b89009eca922ffabaae489134335fbf1
TARUN KUSHWAHA <98316006+375tarun@users.noreply.github.com>
2024-07-23 12:42:42 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port:3000,
     proxy:{
       "/api":{
-        target:"http://localhost:5000",
+        target:"https://my-social-backend-wmcj.onrender.com",
         changeOrigin:true,
       }
     }
