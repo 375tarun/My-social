0ba5d52b9a2739a8277dacafb91b4c8076205cba
TARUN KUSHWAHA <98316006+375tarun@users.noreply.github.com>
2024-07-24 22:58:10 +0530

    Update auth.controller.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -88,7 +88,9 @@ export const login = async (req, res) => {
 
 export const logout = async (req, res) => {
 	try {
-		 res.clearCookie("jwt", {path: '/'});
+		res.cookie("jwt", "", { maxAge: 0, httpOnly: true, 
+		sameSite: "none", 
+		secure: process.env.NODE_ENV !== "development", })
 		res.status(200).json({ message: "Logged out successfully" });
 	} catch (error) {
 		console.log("Error in logout controller", error.message);
